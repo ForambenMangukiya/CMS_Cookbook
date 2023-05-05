678957e9bd9af84c546273abdf110e711d87cd90
Mohammad Awais, Rana <mundarana5577@gmail.com>
2023-05-05 10:31:11 +0200

    footer_css

--- src/Components/footer/FooterLeft.jsx
+++ src/Components/footer/FooterLeft.jsx
@@ -4,16 +4,16 @@ export default function FooterLeft(){
     <div className="footer_left">
       <h3 className="footer_contact">CONTACT US</h3>
       <form className="footer_conact-form">
-        <label for="name">Name</label>
+        <label className="footer_conact-form_lable" for="name">Name</label>
         <input className="footer_input" type="text" id="name" name="name" placeholder="Enter your name" />
 
-        <label for="email">Email</label>
+        <label className="footer_conact-form_lable" for="email">Email</label>
         <input className="footer_input" type="email" id="email" name="email" placeholder="Enter your email" />
 
-        <label for="phone">Phone</label>
+        <label className="footer_conact-form_lable" for="phone">Phone</label>
         <input className="footer_input" type="phone" id="phone" name="phone" placeholder="Enter your phone" />
 
-        <label for="message">Message</label>
+        <label className="footer_conact-form_lable" for="message">Message</label>
         <textarea  className="footer_input" type="text" id="message" name="message" placeholder="Enter your message"></textarea>
 
         <button className="footer_submit" type="submit">Submit</button>
