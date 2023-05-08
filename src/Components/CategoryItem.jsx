247ef317da3048722bc8e43ae754ffe89d43b79f
Adam <adamna1989@gmail.com>
2023-05-08 14:10:41 +0100

    fixed img size on categories page

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -7,7 +7,7 @@ export default function CategoryItem ({name, image, id}) {
 
     return (
         <Grid item xs={6} md={4} xl={3} className='item' sx={{ mb: 1}}>
-            <Link to={`/singlerecipe/${id}`}>
+            <Link to={`/singlerecipe/${id}`} state={{ propsData }}>
 
         <Card className='item' >
             <CardMedia component="img" src={image}/>
